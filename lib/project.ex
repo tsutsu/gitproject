be729cfa8fa9b0673d754adacc86445268010ace
Levi Aul <levi@leviaul.com>
2014-10-08 13:35:32 -0700

    Expose project_version to local scope when module is used

--- lib/project.ex
+++ lib/project.ex
@@ -1,5 +1,12 @@
 defmodule Git.Project do
-  defmacro version do
+  defmacro __using__(_) do
+    quote do
+      require Git.Project
+      import Git.Project
+    end
+  end
+
+  defmacro project_version do
     case File.read("rel/checkout.describe") do
       {:ok, description_lns} -> String.rstrip(description_lns)
       _ -> "0.0.0-untagged"
